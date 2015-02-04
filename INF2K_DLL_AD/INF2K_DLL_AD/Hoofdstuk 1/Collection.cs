e29ab6057dd84e6a4ef109da182ecca2641f8590
RobbinSnippe <robbinsnippe1990@hotmail.com>
2015-02-04 12:56:31 +0100

    Collection klasse toegevoegd

---
+++ INF2K_DLL_AD/INF2K_DLL_AD/Hoofdstuk 1/Collection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2K_DLL_AD.Hoofdstuk_1
+{
+    public class Collection : CollectionBase
+    {
+        public void Add(Object item)
+        {
+            InnerList.Add(item);
+        }
+
+        public void Remove(Object item)
+        {
+            InnerList.Remove(item);
+        }
+
+        public new void Clear()
+        {
+            InnerList.Clear();
+        }
+        public new int Count()
+        {
+            return InnerList.Count;
+        }
+
+        class chapter1
+        {
+            static void Main()
+            {
+                Collection names = new Collection();
+                names.Add("David");
+                names.Add("Bernica");
+                names.Add("Raymond");
+                names.Add("Clayton");
+                foreach (Object name in names)
+                    Console.WriteLine(name);
+                Console.WriteLine("Number of names: " + names.
+                Count());
+                names.Remove("Raymond");
+                Console.WriteLine("Number of names: " + names.
+                Count());
+                names.Clear();
+                Console.WriteLine("Number of names: " + names.
+                Count());
+            }
+        }
+
+    }
+}
