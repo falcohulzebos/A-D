23f73de65103b4f8159fdb3503869260dd7b2a4b
PatrickPieper <patrick.pieper@student.stenden.com>
2015-02-04 14:05:27 +0100

    Aanmaken Test class voor opgave 1

---
+++ INF2K_DLL_AD/INF2K_DLL_AD/Hoofdstuk 1/Opgaven/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace INF2K_DLL_AD.Hoofdstuk_1.Opgaven
+{
+    class Test
+    {
+
+    }
+}
