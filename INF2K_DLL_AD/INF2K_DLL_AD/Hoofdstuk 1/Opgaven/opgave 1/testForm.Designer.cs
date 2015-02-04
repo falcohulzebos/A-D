ddc6f1820b6fdcecaaf73e0350a381787e01bb02
falcohulzebos <falcohulzebos@gmail.com>
2015-02-04 16:12:06 +0100

    turnintest werkend?

--- INF2K_DLL_AD/INF2K_DLL_AD/Hoofdstuk 1/Opgaven/opgave 1/testForm.Designer.cs
+++ INF2K_DLL_AD/INF2K_DLL_AD/Hoofdstuk 1/Opgaven/opgave 1/testForm.Designer.cs
@@ -28,11 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.turnInTest = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.numberBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // turnInTest
+            // 
+            this.turnInTest.Location = new System.Drawing.Point(73, 82);
+            this.turnInTest.Name = "turnInTest";
+            this.turnInTest.Size = new System.Drawing.Size(93, 39);
+            this.turnInTest.TabIndex = 0;
+            this.turnInTest.Text = "turn test in";
+            this.turnInTest.UseVisualStyleBackColor = true;
+            this.turnInTest.Click += new System.EventHandler(this.turnInTest_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(411, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(510, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(73, 127);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 20);
+            this.nameBox.TabIndex = 4;
+            // 
+            // numberBox
+            // 
+            this.numberBox.Location = new System.Drawing.Point(73, 153);
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(100, 20);
+            this.numberBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "number";
+            // 
+            // testForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 452);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.turnInTest);
+            this.Name = "testForm";
             this.Text = "testForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button turnInTest;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox numberBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
