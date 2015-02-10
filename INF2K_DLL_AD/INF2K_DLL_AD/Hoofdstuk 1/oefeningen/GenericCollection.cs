c0630ca653917c03fa0b8ddaaaa555ac4037bbb0
Chris <chris.vinke@hotmail.com>
2015-02-10 11:19:54 +0100

    sadasd

---
+++ INF2K_DLL_AD/INF2K_DLL_AD/Hoofdstuk 1/oefeningen/GenericCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2K_DLL_AD.Hoofdstuk_1.oefeningen
+{
+    class GenericCollection
+    {
+            public interface ICollection<T> : IEnumerable<T>
+        {
+            void Add(T item);
+            bool Remove(T item);
+            void Clear();
+            bool Contains(T item);
+            int Count { get; }
+        }
+    }
+}
