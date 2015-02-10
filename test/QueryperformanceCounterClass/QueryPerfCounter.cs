8d93cd3b37722a3f353eece7b00474efd2b1a3a6
PatrickPieper <patrick.pieper@student.stenden.com>
2015-02-10 13:29:45 +0100

    Form aanmaken voor de Timer class

---
+++ test/QueryperformanceCounterClass/QueryPerfCounter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TestForm
+{
+    public class QueryPerfCounter
+    {
+        [DllImport("KERNEL32")]
+        private static extern bool QueryPerformanceCounter(
+          out long lpPerformanceCount);
+
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+
+        private long start;
+        private long stop;
+        private long frequency;
+        Decimal multiplier = new Decimal(1.0e9);
+
+        public QueryPerfCounter()
+        {
+            if (QueryPerformanceFrequency(out frequency) == false)
+            {
+                // Frequency not supported
+               
+            }
+        }
+
+        public void Start()
+        {
+            QueryPerformanceCounter(out start);
+        }
+
+        public void Stop()
+        {
+            QueryPerformanceCounter(out stop);
+        }
+
+        public double Duration(int iterations)
+        {
+            return ((((double)(stop - start) * (double)multiplier) / (double)frequency) / iterations);
+        }
+    }
+}
