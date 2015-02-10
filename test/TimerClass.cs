f83e7a3717adc51051365cfa640e90bfb6b4763d
PatrickPieper <patrick.pieper@student.stenden.com>
2015-02-10 11:27:53 +0100

    Timer Class gemaakt

--- test/TimerClass.cs
+++ test/TimerClass.cs
@@ -3,10 +3,31 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Class1
+public class Timing
 {
-	public Class1()
+    TimeSpan startingTime;
+    TimeSpan duration;
+
+	public Timing()
 	{
-		
+        startingTime = new TimeSpan(0);
+        duration = new TimeSpan(0);
 	}
+
+    public void stopTime()
+    {
+        duration = Process.GetCurrentProcess().Threads[0].UserProcessorTime.Subtract(startingTime);
+    }
+
+    public void startTime()
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        startingTime = Process.GetCurrentProcess().Threads[0].UserProcessorTime;
+    }
+
+    public TimeSpan Result()
+    {
+        return duration;
+    }
 }
