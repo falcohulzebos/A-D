659d16c4da4aa109910444bc5fca2830e124d518
Chris <chris.vinke@hotmail.com>
2015-02-11 10:23:35 +0100

    GradeForm aangemaakt H2

---
+++ INF2K_DLL_AD/INF2K_DLL_AD/Hoofdstuk 2/GradeTracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Array;
+
+namespace INF2K_DLL_AD.Hoofdstuk_2
+{
+    class GradeTracker
+    {
+        int[] grades= { 12, 56, 89, 65, 61, 36, 45, 23 };
+
+        public void getMaxGrade()
+        {
+            int max = grades.Max();
+        }
+
+        public void getMinGrade()
+        {
+            int min = grades.Min();
+        }
+
+        public void getAverageGrade()
+        {
+            double average = grades.Average();
+        }
+    }
+}
