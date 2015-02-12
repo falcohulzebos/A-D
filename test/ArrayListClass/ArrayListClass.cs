b47fb389a6ffc5ae9047c6bd219aa85eadafc576
Chris <chris.vinke@hotmail.com>
2015-02-12 11:42:07 +0100

    ArrayListClass

---
+++ test/ArrayListClass/ArrayListClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListClass
+{
+    class ArrayListClass
+    {
+        static void main()
+        {
+            int[] myArray = new int[2];
+            myArray[0] = 1;
+            myArray[1] = 2;
+        }
+
+        public void ResizeArray()
+        {
+            myArray.Resize();
+        }
+    }
+}
