b3f536b1bb776d6333251517a4a6af6b0f1cd92d
falcohulzebos <falcohulzebos@gmail.com>
2015-02-19 12:09:23 +0100

    sequential search methods gefixt

--- hoofdstuk 4/hoofdstuk 4/SearchClass.cs
+++ hoofdstuk 4/hoofdstuk 4/SearchClass.cs
@@ -12,38 +12,39 @@ namespace hoofdstuk_4
         {
             int theValue = -1;
             int occuranceCounter = 0;
+            Boolean notFinished = true;
             int i;
-            for (i = 0; i < arr.Length - 1; i++)
+            for (i = 0; i < arr.Length - 1 & notFinished == true; i++)
             {
                 if (arr[i] == value)
                 {
                     occuranceCounter++;
+
                     if (occuranceCounter == occurance)
                     {
                         theValue = arr[i];
+                        notFinished = false;
                     }
                 }
             }
-            return "value: " + theValue + " index: "+ i ;
+            return "value: " + theValue + " index: "+ (i-1) ;
         }
 
         public string seqLast(int[] arr, int value)
         {
-            int theValue = -1;
-            int occurranceCounter = 0;
-            int i;
-            for (i = 0; i < arr.Length - 1; i++)
+            int theValue = -1, i, theIndex = 0;
+
+            for (i = 0; i < arr.Length - 1  ; i++)
             {
                 if (arr[i] == value)
                 {
-                    occurranceCounter++;
-                    if (occurranceCounter > arr.Length)
-                    {
-                        theValue = arr[i];
-                    }
+
+                    theIndex = i;
+                    theValue = arr[i];
+                  
                 }
             }
-            return "value: " + theValue + " index: " + i;
+            return "value: " + theValue + " index: " + theIndex;
         }
 
     }
