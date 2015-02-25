39affeae4a62e4d19f4deea9814b5be5bcda901c
Chris <chris.vinke@hotmail.com>
2015-02-25 10:04:16 +0100

    Piglatin generator

--- Hoofdstuk 7/Program.cs
+++ Hoofdstuk 7/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace Hoofdstuk_7
 {
@@ -10,6 +10,28 @@ namespace Hoofdstuk_7
     {
         static void Main(string[] args)
         {
+            const string SPACE = " ";
+            string pig = "";
+            string first;
+            string last;
+            string txtEnglish = "This is a piglatin generator";
+            string[] words = txtEnglish.Split(' ');
+            foreach (string word in words)
+            {
+                first = word.Substring(0, 1);
+                last = word.Substring(1, word.Length - 1);
+                bool isVowel = "aeiouAEIOU".IndexOf(first) >= 0;
+                if (isVowel)
+                {
+                    pig += word + "way" + SPACE;
+                }
+                else
+                {
+                    pig += last + first + "ay" + SPACE;
+                }
+            }
+            Console.WriteLine(pig);
+            Console.ReadKey();
         }
     }
 }
