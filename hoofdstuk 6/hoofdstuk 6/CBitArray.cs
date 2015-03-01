76483d566b722dedee02e406aed67b56124c3373
falcohulzebos <falcohulzebos@gmail.com>
2015-03-01 11:38:52 +0100

    CBitArray aangepast

--- hoofdstuk 6/hoofdstuk 6/CBitArray.cs
+++ hoofdstuk 6/hoofdstuk 6/CBitArray.cs
@@ -10,6 +10,34 @@ namespace hoofdstuk_6
     class CBitArray
     {
         BitArray BArray;
+        Boolean[] boolArray;
+
+        public CBitArray(Boolean[] a)
+        {
+            boolArray = new Boolean[a.Length];
+            boolArray = a;
+        }
+
+        public void toBArray(Boolean[] b)
+        {
+            int position = 0;
+            
+        }
+
+        public int boolToBit(Boolean b)
+        {
+            int returnByte = -1;
+            if(b == true)
+            {
+                returnByte = 1;
+            }
+            if(b == false)
+            {
+                returnByte = 0;
+            }
+
+            return returnByte;
+        }
 
 
     }
