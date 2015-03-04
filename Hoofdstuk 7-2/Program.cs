bc17958e16002087d49c6510fd07b97d76e02006
Chris <chris.vinke@hotmail.com>
2015-03-04 20:32:07 +0100

    Hoofdstuk 7 af

--- Hoofdstuk 7-2/Program.cs
+++ Hoofdstuk 7-2/Program.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Hoofdstuk_7_2
 {
     class Program
     {
+        //public static void Main()
+        //{
+        //    string pattern = "patterns";
+        //    string input = "er zijn veel patterns in deze tekst, maar de patterns hebben bepaalde patterns die je kan vinden";
+        //    int occurenceCounter = 0;
+
+        //    foreach (Match m in Regex.Matches(input, pattern))
+        //    {
+        //        occurenceCounter++;
+        //    }
+        //    Console.WriteLine(occurenceCounter);
+        //    Console.ReadLine();
+        //}
+
         public static void Main()
         {
             string pattern = "patterns";
-            string input = "er zijn veel patterns in deze tekst, maar de patterns hebben bepaalde patterns die je kan vinden";
+            var sb = new StringBuilder("er zijn veel patterns in deze tekst, maar de patterns hebben bepaalde patterns die je kan vinden");
             int occurenceCounter = 0;
-
+            string input = sb.ToString();
             foreach (Match m in Regex.Matches(input, pattern))
             {
                 occurenceCounter++;
---
+++ Hoofdstuk 7-4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoofdstuk_7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nounArray = new string[] { "I", "you", "he", "she", "Mary" };
+            string[] verbArray = new string[] { "ate", "bought", "saw", "sold", "wanted" };
+            string[] objeArray = new string[] { "apple", "banana", "pear", "peach", "melon" };
+            var sb = new StringBuilder("ate Mary peach");
+            string[] sentenceArray = sb.ToString().Split(' ');
+            string noun = "", verb = "", obje = "";
+            for (int i = 0; i < sentenceArray.Length; i++)
+            {
+                for (int j = 0; j < nounArray.Length; j++)
+                {
+                    if (sentenceArray[i] == nounArray[j])
+                    {
+                        noun = sentenceArray[i];
+                    }
+
+                    if (sentenceArray[i] == verbArray[j])
+                    {
+                        verb = sentenceArray[i];
+                    }
+
+                    if (sentenceArray[i] == objeArray[j])
+                    {
+                        obje = sentenceArray[i];
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("Noun: " + noun);
+            Console.WriteLine("Verb: " + verb);
+            Console.WriteLine("Object: " + obje);
+            Console.ReadLine();
+        }
+    }
+}
