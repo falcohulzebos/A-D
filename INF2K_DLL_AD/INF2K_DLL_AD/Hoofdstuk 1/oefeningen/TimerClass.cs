b7b2e488132a376544223dc1b1957cb6039c4c76
falcohulzebos <falcohulzebos@gmail.com>
2015-03-25 11:06:12 +0100

    timerclass toegevoegd met processor affinity

---
+++ INF2K_DLL_AD/INF2K_DLL_AD/Hoofdstuk 1/oefeningen/TimerClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace INF2K_DLL_AD.Hoofdstuk_1.oefeningen
+{
+    public class TimerClass
+    {
+        TimeSpan startingTime;
+        TimeSpan duration;
+
+        public TimerClass()
+        {
+            startingTime = new TimeSpan(0);
+            duration = new TimeSpan(0);
+        }
+
+        public void stopTime()
+        {
+            Process p = Process.GetCurrentProcess();
+            ProcessThread thread = p.Threads[0];
+            thread.ProcessorAffinity = (IntPtr)1;
+            duration = thread.UserProcessorTime.Subtract(startingTime);
+        }
+
+        public void startTime()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Process p = Process.GetCurrentProcess();
+            ProcessThread thread = p.Threads[0];
+            thread.ProcessorAffinity = (IntPtr)1;
+            startingTime = thread.UserProcessorTime;
+            
+        }
+        public TimeSpan result()
+        {
+            return duration;
+        }
+    }
+}
---
+++ INF2K_DLL_AD/INF2K_DLL_AD/QueryPerfCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace INF2K_DLL_AD
+{ //
+    public class QueryPerfCounter
+    {
+        [DllImport("KERNEL32")]
+        private static extern bool QueryPerformanceCounter(
+          out long lpPerformanceCount);
+
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+
+        private long start;
+        private long stop;
+        private long frequency;
+        Decimal multiplier = new Decimal(1.0e9);
+
+        public QueryPerfCounter()
+        {
+            if (QueryPerformanceFrequency(out frequency) == false)
+            {
+                // Frequency not supported
+               
+            }
+        }
+
+        public void Start()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            QueryPerformanceCounter(out start);
+        }
+
+        public void Stop()
+        {
+            QueryPerformanceCounter(out stop);
+        }
+
+        public double Duration(int iterations)
+        {
+            return ((((double)(stop - start) * (double)multiplier) / (double)frequency) / iterations);
+        }
+    }
+}
