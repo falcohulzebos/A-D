924ff7fb887d54929d446e8ad0cfb19cc9f46528
falcohulzebos <falcohulzebos@gmail.com>
2015-03-26 11:42:47 +0100

    priorityQueue af

--- INF2K_DLL_AD/INF2K_DLL_AD/search/searchClass.cs
+++ INF2K_DLL_AD/INF2K_DLL_AD/search/searchClass.cs
@@ -8,10 +8,18 @@ namespace INF2K_DLL_AD.search
 {
     class searchClass<T>
     {
-        public T seqSearch(T[] arr, T value, int occurance)
+        /// <summary>
+        /// Performs sequential search on the array
+        /// </summary>
+        /// <param name="arr">Array to search</param>
+        /// <param name="value">value to search for</param>
+        /// <param name="occurance">occurance wich must be found</param>
+        /// <returns>int index</returns>
+        public int seqSearch(T[] arr, T value, int occurance)
         {
             T theValue = default(T);
             int occuranceCounter = 0;
+            int index = -1; ;
             Boolean notFinished = true;
             int i;
             for (i = 0; i < arr.Length - 1 & notFinished == true; i++)
@@ -23,13 +31,19 @@ namespace INF2K_DLL_AD.search
                     if (occuranceCounter == occurance)
                     {
                         theValue = arr[i];
+                        index = i;
                         notFinished = false;
                     }
                 }
             }
-            return theValue;
+            return index;
         }
 
+        /// <summary>
+        /// searches for lowest value in array
+        /// </summary>
+        /// <param name="arr">Array to search</param>
+        /// <returns>min value</returns>
         public T Min(T[] arr)
         {
             T theValue = default(T);
@@ -44,6 +58,11 @@ namespace INF2K_DLL_AD.search
             return theValue;
         }
 
+        /// <summary>
+        /// searches for highest value in array
+        /// </summary>
+        /// <param name="arr">Array to search</param>
+        /// <returns>max value</returns>
         public T Max(T[] arr)
         {
             T theValue = default(T);
@@ -58,11 +77,13 @@ namespace INF2K_DLL_AD.search
             return theValue;
         }
 
-        //de functie voor het binair zoeken
+        /// <summary>
+        /// searches for value in array with binary algorithm
+        /// </summary>
+        /// <param name="arr">Array to search</param>
+        /// <returns>min value</returns>
         public int binSearch(T[] arr, T value)
         {
-            
-           
             int upperBound, lowerBound, mid;
 
             upperBound = arr.Length;
