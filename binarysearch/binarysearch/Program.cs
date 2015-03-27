04e5f4b2ed705e013e5c5e87c4193f7d970c9b41
PatrickPieper <patrick.pieper@student.stenden.com>
2015-03-27 11:27:42 +0100

    binarysearch

---
+++ binarysearch/binarysearch/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class Node
+{
+    public int data;
+    public Node left;
+    public Node right;
+
+
+
+    public void displayNode()
+    {
+        Console.WriteLine(data + " ");
+    }
+
+
+}
+
+public class binarySearchTree
+{
+    public Node root;
+
+    public binarySearchTree()
+    {
+        root = null;
+    }
+
+    public void insert(int i)
+    {
+        Node newNode = new Node();
+        newNode.data = i;
+
+        if (root == null)
+        {
+            root = newNode;
+        }
+        else
+        {
+            Node current = root;
+            Node parrent;
+
+            while (true)
+            {
+                parrent = current;
+
+                if (i < current.data)
+                {
+                    current = current.left;
+
+                    if (current == null)
+                    {
+                        parrent.left = newNode;
+
+                        break;
+                    }
+                    else
+                    {
+                        current = current.right;
+
+                        if (current == null)
+                        {
+                            parrent.right = newNode;
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
