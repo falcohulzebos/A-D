22ea8e7155957bf5fd7e2a0e928f3ba45f02d834
RobbinSnippe <robbinsnippe1990@hotmail.com>
2015-04-07 11:16:51 +0200

    comments circlinklist

--- CircularList/CircularList/Program.cs
+++ CircularList/CircularList/Program.cs
@@ -31,13 +31,13 @@ class Program
         }
     }
 
-
+    // Node klasse waar de data in op wordt geslagen. Deze nodes worden in een array gezet.
     public class Node 
     {
 
-        public Object Element;
-        public Node next;
-        public Node previous;
+        public Object Element;  // inhoud van de node
+        public Node next;       // de aanliggende node
+        public Node previous;   // de vorige node
 
         public Node() 
         {
@@ -56,9 +56,9 @@ class Program
 
     public class LinkedList {
 
-        public Node current;
-        protected Node header;
-        private int count;
+        public Node current;       // de huidige node
+        protected Node header;     // de eerste node in de list
+        private int count;         // het aantal objecten in de list
 
         public LinkedList() 
         {
@@ -68,23 +68,26 @@ class Program
            
         }
 
+        // checken of de list leeg is.
         public bool IsEmpty() 
         {
             return (header.next == null);
         }
 
+        // de plek naast de header leegmaken
         public void MakeEmpty() 
         {
             header.next = null;
         }
 
+        // Alle inhoud van de list printen in de console
         public void PrintList() 
         {
             Node current = new Node();
             current = header;
             int counter = 0;
 
-
+            // zolang de volgende node geen "header" is, print dan de volgende node uit in de console
             while (!(current.next.Element.ToString() == "header")) 
             {
                 Console.WriteLine(counter + ": "+ current.next.Element);
@@ -93,11 +96,16 @@ class Program
             }
         }
 
+        // De Node opzoeken die 1 plek voor de ingegeven node ligt.
         private Node FindPrevious(Object n) 
         {
+            // beginnen bij de node na de header
             Node current = header.next;
             Console.WriteLine("current " + current.Element.ToString());
             
+            // Als de volgende node niet leeg is en als de volgende node niet de ingevulde parameter is
+            // dan wordt current de volgende node en herhaalt de loop zich
+            // is de volgende node wel de ingevulde parameter, dan wordt de huidige node uitgeprint.
             while (!(current.next == null) && current.next.Element !=n)
             {
                
@@ -107,11 +115,14 @@ class Program
             return current;
          }
 
+        // zoeken naar de ingegeven node.
         private Node Find(Object n) 
         {
+            // beginnen bij de header
             Node current = new Node();
             current = header;
 
+            // zolang current niet de ingegeven parameter is wordt de volgende node bekeken.
             while (current.Element != n)
             {
                 current = current.next;
@@ -119,47 +130,59 @@ class Program
             return current;
         }
 
+        // verwijderen van de ingegeven node
         public void Remove(Object n) 
         {
+            // eerst wordt er gekeken naar de node die voor de ingegeven node ligt.
             Node p = FindPrevious(n);
             Console.WriteLine("previous= " + p.Element.ToString());
-            try { 
-            if (!(p.next == null))
-            {
-                p.next = p.next.next;
-            }
-            count--;
-            }
-            catch(Exception)
-            {}
+            try 
+            { 
+                // als de node na de "vorige" node geen null is
+                // dan wordt de ingegeven node vervangen door de volgende node
+                // en wordt de count met 1 naar beneden gehaald.
+                if (!(p.next == null))
+                {
+                 p.next = p.next.next;
+                }
+                count--;
+                }
+                catch(Exception)
+                {}
         }
 
+        // toevoegen van een entry.
+        // n1: de toe te voegen node
+        // n2: de node waarnaast n1 moet worden toegevoegd.
         public void Insert(Object n1, Object n2) 
         {
             Node current = new Node();
-            Node newnode = new Node(n1);
-            current = Find(n2);
-            newnode.next = current.next;
-            current.next = newnode;
+            Node newnode = new Node(n1);    // de node om toe te voegen
+            current = Find(n2);             // current wordt de node waarnaast n1 moet worden toegevoegd.
+            newnode.next = current.next;    // nieuwe lege node naast de nieuwe node toevoegen
+            current.next = newnode;         // node naast de huidige node wordt de nieuwe node.
            
             count++;
         }
 
+        // een node aan het begin toevoegen
         public void InsertFirst(Object n) 
         {
             Node current = new Node(n);
-            current.next = header;
-            header.next = current;
+            current.next = header;      // current.next wordt "header"
+            header.next = current;      // nieuwe node wordt na header toegevoegd.
            
             count++;
 
         }
 
+        // alle nodes een n aantal plekken opschuiven.
         public Node Move(int n) 
         {
             Node current = header.next;
             Node temp;
 
+            
             for(int i = 0; i <= n; i++)
             {
                 current = current.next;
